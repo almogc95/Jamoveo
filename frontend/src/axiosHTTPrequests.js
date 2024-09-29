8537507b3d4ed79f706618265d64836c0a286e12
almog <almogc95@gmail.com>
2024-09-29 23:32:56 +0300

    change environment variables

--- frontend/src/axiosHTTPrequests.js
+++ frontend/src/axiosHTTPrequests.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.NODE_ENV === 'production'
-    ? 'https://jamoveo-650e.onrender.com' //react app's deployed URL 
-    : 'http://localhost:8080';
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
 
 const axiosHTTPrequests = axios.create({
     baseURL: API_URL,
