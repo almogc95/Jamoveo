ff6ab4b1c1ae8c944e311783ac597bebec92c25e
almog <almogc95@gmail.com>
2024-10-01 11:01:57 +0300

    deploy

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,12 @@ mongoose.connection.on('error', (err) => {
     console.error(`MongoDB connection error: ${err}`);
 });
 
-
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+})
 app.use(cors({
     origin: 'https://jamoveo-frontend-s3iw.onrender.com',
     methods: ['GET', 'POST'],
