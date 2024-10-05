3aeed4beb9bddef9841505702e1050e7acc0126c
almog <almogc95@gmail.com>
2024-10-05 18:53:40 +0300

    fix axios

--- frontend/src/axiosHTTPrequests.js
+++ frontend/src/axiosHTTPrequests.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 //TODO
-// let API_URL;
+let API_URL;
 
-// process.env.PROJECT_MODE === 'production' ?
-//     API_URL = process.env.REACT_APP_BACKEND_URL :
-//     API_URL = 'http://localhost:8080'
+process.env.PROJECT_MODE === 'production' ?
+    API_URL = process.env.REACT_APP_BACKEND_URL :
+    API_URL = 'http://localhost:8080'
 
 const axiosHTTPrequests = axios.create({
-    baseURL: 'https://jamoveo-backend-al1u.onrender.com'
+    baseURL: API_URL
 });
 
 export default axiosHTTPrequests;
