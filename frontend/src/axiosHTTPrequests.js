05e4797ced7461c841db8c8701f006c8bc1a820d
almog <almogc95@gmail.com>
2024-10-06 21:50:57 +0300

    fix axios file

--- frontend/src/axiosHTTPrequests.js
+++ frontend/src/axiosHTTPrequests.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
 
-const API_URL = process.env.PROJECT_MODE === 'production' ?
-    API_URL = process.env.REACT_APP_BACKEND_URL :
-    API_URL = 'http://localhost:8080'
+const API_URL = process.env.PROJECT_MODE === 'production'
+    ? API_URL = process.env.REACT_APP_BACKEND_URL
+    : API_URL = 'http://localhost:8080'
 
 const axiosHTTPrequests = axios.create({
     baseURL: API_URL
 });
+console.log('API_URL:', API_URL);
 
 export default axiosHTTPrequests;
